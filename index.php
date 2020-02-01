6d9c1aa54ff4ab8c71f2efdf4a28b5a3032352c3
KucingMaboek <45931684+KucingMaboek@users.noreply.github.com>
2020-02-01 19:14:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "macd-server.database.windows.net";
+    $user = "kucingmaboek";
+    $pass = "Bt2pdf?@g0j6";
+    $db = "macdDB";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
